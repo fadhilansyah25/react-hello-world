a81b32f61d4c7b82beebae83158051e0d419f35f
fadhilansyah25 <fadhilansyah25@gmail.com>
2020-10-04 19:58:20 +0700

    11.Interaksi dengan Back-End, Pemanggilan API (GET) Part 2 - #9-2 ReactJS Tutorial [Indonesia]

--- src/component/BlogPost/BlogPost.jsx
+++ src/component/BlogPost/BlogPost.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import '../BlogPost/BlogPost.css';
 import Post from '../Post/Post';
 
 export default class BlogPost extends Component {
+    state = {
+        post: []
+    }
+
+    componentDidMount() {
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        //     .then(response => response.json())
+        //     .then(json => {
+        //         this.setState({
+        //             post: json,
+        //         });
+        //         console.log(this.state.post.slice(0, 10));
+        //     })
+
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            this.setState({
+                post: response.data,
+            });
+            console.log(response);
+        });
+    }
+
     render() {
         return (
-            <div>
-                <Post title="title" desc="desc" />
+            <div className="container d-flex flex-wrap">
+                {
+                    this.state.post.slice(0, 8).map(post => {
+                        return <Post key={post.id} title={post.title} desc={post.body} />
+                    })
+                }
             </div>
         )
     }

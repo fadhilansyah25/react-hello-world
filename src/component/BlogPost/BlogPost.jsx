5d89f5e8dcb8287985a7617aaf1faf483b72bae7
fadhilansyah25 <fadhilansyah25@gmail.com>
2020-10-07 21:13:20 +0700

    16.Interaksi dengan Back-End, Pemanggilan API (PUT)  Part 7 - #9-7 ReactJs

--- src/component/BlogPost/BlogPost.jsx
+++ src/component/BlogPost/BlogPost.jsx
@@ -12,7 +12,8 @@ export default class BlogPost extends Component {
             id : 1,
             title : "",
             body : ""
-        }
+        },
+        update : false
     }
 
     componentDidMount() {
@@ -46,7 +47,10 @@ export default class BlogPost extends Component {
         let formBlogPostNew = {...this.state.formBlogPost};
         let timestamp  = new Date().getTime();
 
-        formBlogPostNew['id'] = timestamp;
+        if (!this.state.update) {
+            formBlogPostNew['id'] = timestamp;
+        }
+
         formBlogPostNew[event.target.name] = event.target.value;
 
         this.setState({
@@ -55,19 +59,58 @@ export default class BlogPost extends Component {
     }
 
     handleClickSubmit = () => {
-        console.log(this.state.formBlogPost);
-        this.postDataToAPI();
+        // console.log(this.state.formBlogPost);
+        if (this.state.update) {
+            this.putDataToAPI();
+        } else {
+            this.postDataToAPI();
+        }
     }
 
     postDataToAPI = () => {
         axios.post('http://localhost:3004/posts', this.state.formBlogPost )
         .then((res) => {
             console.log(res);
-            // this.getPostAPI();
+            this.getPostAPI();
+            this.setState({
+                formBlogPost: {
+                    userId : 1,
+                    id : 1,
+                    title : "",
+                    body : ""
+                }
+            })
         }, (err) => {
             console.log('error : ', err);
         })
     }
+
+    putDataToAPI = () => {
+        axios.put(`http://localhost:3004/posts/${this.state.formBlogPost.id}`, this.state.formBlogPost)
+        .then((res) => {
+            console.log(res);
+            this.getPostAPI();
+            this.setState({
+                formBlogPost: {
+                    userId : 1,
+                    id : 1,
+                    title : "",
+                    body : ""
+                },
+                update : false
+            })
+        })
+    }
+
+    handleUpdate = (data) => {
+        console.log(data);
+        this.setState({
+            formBlogPost: data,
+            update : true
+        })
+    }
+
+
 
     render() {
         return (
@@ -76,19 +119,19 @@ export default class BlogPost extends Component {
                     <form>
                         <div className="form-group mb-3">
                             <label htmlFor="title" className="mb-1">Title</label>
-                            <input type="text" className="form-control" name="title" placeholder="Add Title" onChange={this.handleFormChange} />
+                            <input type="text" value={this.state.formBlogPost.title} className="form-control" name="title" placeholder="Add Title" onChange={this.handleFormChange} />
                         </div>
                         <div className="form-group mb-3">
                             <label htmlFor="body" className="mb-1">Blog Content</label>
-                            <textarea className="form-control" name="body" rows="10" placeholder="Add Blog Content" onChange={this.handleFormChange}></textarea>
+                            <textarea className="form-control" value={this.state.formBlogPost.body} name="body" rows="10" placeholder="Add Blog Content" onChange={this.handleFormChange}></textarea>
                         </div>
-                        <button className="btn btn-outline-primary" onClick={this.handleClickSubmit}>Submit</button>
+                        <button type="button" className="btn btn-outline-primary" onClick={this.handleClickSubmit}>Submit</button>
                     </form>
                 </div>
                 <div className="d-flex flex-wrap">
                     {
                         this.state.post.slice(90).reverse().map(post => {
-                            return <Post key={post.id} data={post} remove={this.handleRemove} />
+                            return <Post key={post.id} data={post} remove={this.handleRemove} update={this.handleUpdate} />
                         })
                     }
                 </div>
--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -8,7 +8,10 @@ const Post = (props) => {
                 <h5 className="card-title">{props.data.title}</h5>
                 <p className="card-text">{props.data.body}</p>
             </div>
+            <div className="d-flex">
+            <button onClick={() => props.update(props.data)} className="btn btn-primary update-btn">Update</button>
             <button onClick={() => props.remove(props.data.id)} className="btn btn-danger remove-btn">Remove</button>
+            </div>
         </div>
     )
 }
